db94bacc049f70185930cb4ecb14bb69c326956d
nikantonelli <nik@nikantonelli.com>
2015-02-19 12:23:36 +0000

    Add recursive Test Folder contents to a TestSet
    Runs down the TestFolder tree looking for TestCases and children test
    folders

---
+++ Menu/AddSubFolderMenuItem.js
@@ -0,0 +1,72 @@
+Ext.define('AddSubFolderMenuItem', {
+        extend:  Rally.ui.menu.item.RecordMenuItem ,
+        alias: 'widget.rallyrecordmenuitemaddsubfolder',
+
+        config: {
+            text: 'Add folder...',
+            cls: 'icon-folder'
+            /**
+             * @cfg handler
+             */
+
+            /**
+             * @cfg predicate
+             */
+        },
+
+        constructor: function (config) {
+            config = config || {};
+            config.predicate = config.predicate || Rally.predicate.RecordPredicates.mustPassAllPredicates([
+                Rally.predicate.RecordPredicates.isUpdatable,
+                Rally.predicate.RecordPredicates.canHaveChildren
+            ]);
+            config.handler = config.handler || this._onAddChildClicked;
+
+            this.initConfig(config);
+            this.callParent(arguments);
+        },
+
+        _getChildType: function () {
+            var field = this.record.getField('Children');
+
+            return field.attributeDefinition.AllowedValueType._refObjectName;
+        },
+
+        _getCpoid: function (record) {
+            var project = record.get('Project') || {_ref: record.self.context.project};
+
+            return Rally.util.Ref.getOidFromRef(project._ref);
+        },
+
+        _onAddChildClicked: function () {
+            //This global check feels ALMy and probably shouldn't be here...
+            if (window.detail) {
+                window.detail.refreshContent = false;
+            }
+
+            var childType = this._getChildType(),
+                record = this.record,
+                params = this._buildParamsForEditor(record);
+
+            Rally.data.wsapi.ModelFactory.getModel({
+                type: childType,
+                context: record.self.context
+            }).then(function (ChildModel) {
+                    params.typeDef = ChildModel.typeDefOid;
+                    Rally.nav.Manager.create(childType, params);
+                });
+        },
+
+        _buildParamsForEditor: function (parent) {
+            var params = {
+                parent: parent.get('ObjectID'),
+                parentTypeDef: parent.self.typeDefOid,
+                cpoid: this._getCpoid(parent),
+                Parent: Rally.util.Ref.getOidFromRef(parent.get('_ref'))
+            };
+
+            return params;
+        }
+
+    });
+
---
+++ Menu/AddToTestSetMenuItem.js
@@ -0,0 +1,182 @@
+Ext.define('AddToTestSetMenuItem', {
+        extend:  Rally.ui.menu.item.RecordMenuItem ,
+        alias: 'widget.rallyrecordmenuitemaddtoTS',
+
+        config: {
+            text: 'Add to testset...',
+            cls: 'icon-test-set'
+            /**
+             * @cfg handler
+             */
+
+            /**
+             * @cfg predicate
+             */
+        },
+
+        foldersToScan: [],
+
+        constructor: function (config) {
+            config = config || {};
+            config.predicate = config.predicate || Rally.predicate.RecordPredicates.mustPassAllPredicates([
+                Rally.predicate.RecordPredicates.isUpdatable
+            ]);
+            config.handler = config.handler || this._onAddToTSClicked;
+
+            this.initConfig(config);
+            this.callParent(arguments);
+
+            this.addEvents('syncTarget');
+
+        },
+
+        _addTestCasesForTestFolder: function(source, tcList)
+        {
+            var self = this;
+
+            var sourceCollection = source.getCollection('TestCases');
+            sourceCollection.load().then({
+                success: function() {
+                    tcList = tcList.concat(sourceCollection.getRecords());
+
+                    //Remove this folder from the scanList
+                    _.remove(self.foldersToScan, function(testfolder) {
+                        return testfolder.get('FormattedID') === source.get('FormattedID');
+                    });
+
+                    //At the end of every Promise completion, we need to check to see if we are the last
+                    if (self.foldersToScan) {
+                        if (self.foldersToScan.length === 0) {
+                            //We know all the promises are delivered, we can now notify the app to sync the targetCollection
+
+                            //First get the store from the manager
+                            var store = Ext.data.StoreManager.lookup('wsStore');
+
+                            //And then ping it, so it saves the TC list
+                            var ping = store && store.fireEvent('syncTarget', tcList);
+
+                        } else {
+                            //we have any more, if so, fire off another _scanFolder
+                            self._scanFolder(_.first(self.foldersToScan), tcList);
+
+                        }
+                    }
+                }
+            });
+        },
+
+        _scanFolder: function( source, tcList) {
+
+            var self = this;
+
+            //See if there are any children folders and put them on the wait list
+            var folderCollection = source.getCollection('Children');
+            folderCollection.load().then( {
+                success: function(error, result) {
+                    _.each(folderCollection.data.items, function(record) {
+                        self.foldersToScan.push(record);
+                    });
+
+                //Do this folder's direct TestCases
+                self._addTestCasesForTestFolder(source, tcList);
+
+                },
+                failure: function(error, result) { debugger; }
+            });
+
+        },
+
+
+        _onAddToTSClicked: function () {
+
+            var self = this;
+
+            //This global check feels ALMy and probably shouldn't be here...
+            if (window.detail) {
+                window.detail.refreshContent = false;
+            }
+
+            var chooser = Ext.create('Rally.ui.dialog.SolrArtifactChooserDialog', {
+                artifactTypes: ['testset'],
+                autoShow: true,
+                title: 'Choose Test Set',
+                columns: ['FormattedID', 'Name', 'Iteration'],
+                listeners: {
+                    artifactchosen: function(dialog, selectedRecord){
+
+                        //We have the test set (selectedRecord) and the testitem (self)
+                        //Get the full TestSet item in a store so we can modify it:
+
+                        var itemStore = Ext.create('Rally.data.wsapi.Store', {
+                            model: 'testset',
+                            autoLoad: true,
+                            storeId: 'wsStore', //Log the store with the store manager for later retrieval
+                            targetCollection: null,
+                            fetch: [ 'FormattedID', 'TestCases'],
+                            filters: [
+                                {
+                                    property: 'FormattedID',
+                                    value: selectedRecord.get('FormattedID')
+                                }
+                            ],
+                            listeners: {
+                                load: function(store, records, success) {
+                                        var testcaseList = [];
+
+                                        //Get the collection of current test cases
+                                        this.targetCollection = records[0].getCollection('TestCases');
+
+                                        if (self.record.get('_type') === 'testcase'){
+                                            //Add the single test case to the test set
+                                            testcaseList.add(self.record);
+                                            this.targetCollection.load({
+                                                callback: function(){
+                                                    _.each(testcaseList, function(testcase) {
+                                                        this.targetCollection.add(testcase);
+                                                    });
+                                                    this.targetCollection.sync({
+                                                        success: function() {Rally.ui.notify.Notifier.show({ message: 'Test Case added to Test Set'});},
+                                                        failure: function() {Rally.ui.notify.Notifier.show({ message: 'Failed to add Test Case to Test Set'});}
+                                                    });
+                                                }
+                                            });
+                                        }
+                                        else {
+                                            //Add the contents of the folder recursively
+
+                                        this.targetCollection.load( {
+                                            callback: function() {
+                                                self._scanFolder(self.record, testcaseList);
+                                            }
+                                        });
+                                    }
+                                },
+                                //Our own synchronisation event from the testcase loading
+                                syncTarget: function(testcaseList) {
+
+                                    //Need to refetch the context of the store as we are part of 'window' as we come in
+                                    var store = Ext.data.StoreManager.lookup('wsStore');
+
+                                    _.each(testcaseList, function(testcase) {
+                                        store.targetCollection.add(testcase);
+                                    });
+                                    store.targetCollection.sync({
+                                        success: function() {
+                                            Rally.ui.notify.Notifier.show({ message: testcaseList.length + ' Test Cases added to Test Set'});
+                                        },
+                                        failure: function() {
+                                            Rally.ui.notify.Notifier.show({ message: 'Failed to add Test Cases to Test Set'});
+                                        }
+                                    });
+
+                                        }
+                                }
+                        });
+
+                    },
+                    scope: this
+                }
+             });
+        }
+
+    });
