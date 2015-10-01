828518196b5e1260f878f54492eb690899e431e5
nikantonelli <nik@nikantonelli.com>
2015-10-01 09:38:55 -0600

    Move test folders to new parent

--- Tree/TestCaseTree.js
+++ Tree/TestCaseTree.js
@@ -544,6 +544,7 @@
         if (this._isTestFolder(record)){
             //Change the project of the source to the same as the drop target
             record.set('Project', newParentRecord.get('Project')._ref);
+            record.set('Parent', newParentRecord.get('Parent')._ref);
         }
         else if (this._isTestCase(record)){
             record.set('Project', newParentRecord.get('Project')._ref);
